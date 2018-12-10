5b836f4d478017a4e6ffec123599c9070be669bc
Wei Li <liwei72439936@gmail.com>
2018-12-10 15:10:52 +0800

    happy birthday

--- HappyBirthday/app/src/main/java/com/example/weili/happybirthday/MainActivity.java
+++ HappyBirthday/app/src/main/java/com/example/weili/happybirthday/MainActivity.java
@@ -1,4 +1,4 @@
-package com.example.weili.helloworld;
+package com.example.weili.happybirthday;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -10,6 +10,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Log.d("MainActivity", "Hello World");
+        try {
+            throw new RuntimeException();
+        } catch (RuntimeException e) {
+            Log.e("MainActivity", "bad thing happen", e);
+        }
     }
 }
